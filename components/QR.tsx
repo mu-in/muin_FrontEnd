6499352518baf6790f24ffeaa3425d8ad1360809
2kyung19 <2kyung19@gmail.com>
2021-08-24 12:48:03 +0900

    feature : QR code [#3]

---
+++ components/QR.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from 'react';
+import QRCode from 'react-native-qrcode-svg';
+
+function QR(): ReactElement {
+	return <QRCode value="a225f3b3-6ba2-4da1-b14a-1bb691cabf79" />;
+}
+
+export default QR;
