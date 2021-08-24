ee5629c827e7c099b3f1a705e14872e0ab288f3c
2kyung19 <2kyung19@gmail.com>
2021-08-24 18:41:09 +0900

    feature : tab icon [#8]

--- page/App/index.tsx
+++ page/App/index.tsx
@@ -2,11 +2,13 @@ import React, { ReactElement } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Text } from 'react-native-svg';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Home from './Home';
 import StoreList from './StoreList';
 import StoreInfo from './StoreInfo';
 import Manage from './Manage';
+
+Ionicons.loadFont().then();
 
 const manager = true; // manager에만 manage tab 생성
 
@@ -26,7 +28,22 @@ const Tab = createBottomTabNavigator();
 function App(): ReactElement {
 	return (
 		<NavigationContainer>
-			<Tab.Navigator>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ size, color }: { size: number; color: string }) => {
+						let icon = 'ios-information-circle';
+
+						if (route.name === 'QR') {
+							icon = 'ios-scan-sharp';
+						} else if (route.name === 'STORE') {
+							icon = 'basket-outline';
+						} else if (route.name === 'MANAGE') {
+							icon = 'md-cog';
+						}
+						return <Ionicons name={icon} size={size} color={color} />;
+					},
+				})}
+			>
 				<Tab.Screen name="QR" component={Home} />
 				<Tab.Screen name="STORE" component={StoreStackScreen} />
 				{manager === true ? <Tab.Screen name="MANAGE" component={Manage} /> : null}
