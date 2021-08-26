7b7ada465c6be6dc912e211dad6c5e8502e50021
2kyung19 <2kyung19@gmail.com>
2021-08-26 16:35:48 +0900

    feature : calculate totp [#11]

--- components/QR.tsx
+++ components/QR.tsx
@@ -1,8 +1,22 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import QRCode from 'react-native-qrcode-svg';
+import RNTotp from 'react-native-totp';
 
 function QR(): ReactElement {
-	return <QRCode value="a225f3b3-6ba2-4da1-b14a-1bb691cabf79" />;
+	const [token, setToken] = useState('');
+
+	RNTotp.generateOTP(
+		{
+			base32String: 'a225f3b3-6ba2-4da1-b14a-1bb691cabf79',
+			digits: 16,
+			period: 30,
+		},
+		(code) => {
+			setToken(code);
+		}
+	);
+
+	return <QRCode value={token} />;
 }
 
 export default QR;
