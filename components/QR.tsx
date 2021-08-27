0690f16ffc2775a60f0d1268579911d27d338ea8
2kyung19 <2kyung19@gmail.com>
2021-08-27 15:46:16 +0900

    feature : set interval [#11]

--- components/QR.tsx
+++ components/QR.tsx
@@ -1,20 +1,25 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import QRCode from 'react-native-qrcode-svg';
 import RNTotp from 'react-native-totp';
 
 function QR(): ReactElement {
-	const [token, setToken] = useState('');
+	const [token, setToken] = useState('-');
 
-	RNTotp.generateOTP(
-		{
-			base32String: 'a225f3b3-6ba2-4da1-b14a-1bb691cabf79',
-			digits: 16,
-			period: 30,
-		},
-		(code) => {
-			setToken(code);
-		}
-	);
+	useEffect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const generate = setInterval(() => {
+			RNTotp.generateOTP(
+				{
+					base32String: 'a225f3b3-6ba2-4da1-b14a-1bb691cabf79',
+					digits: 8,
+					period: 5,
+				},
+				(code) => {
+					setToken(code);
+				}
+			);
+		}, 5000);
+	}, [token]);
 
 	return <QRCode value={token} />;
 }
