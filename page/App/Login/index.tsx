9b9a3fda9eecd09601059b7ce0583442f9ec6c16
2kyung19 <2kyung19@gmail.com>
2021-10-13 18:35:42 +0900

    feature : link google oauth 이경은천재

--- page/App/Login/index.tsx
+++ page/App/Login/index.tsx
@@ -1,7 +1,9 @@
-import React, { ReactElement, useContext } from 'react';
+import React, { ReactElement, useContext, useState } from 'react';
 import { SafeAreaView, View, Text, StyleSheet, Button } from 'react-native';
 import { ParamListBase } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+
 import { UserContext } from '../Context';
 
 interface Props {
@@ -17,17 +19,34 @@ const styles = StyleSheet.create({
 });
 
 function Login({ navigation }: Props): ReactElement {
-    const { name, setName } = useContext(UserContext) 
-	
-    const login = () => {
-        setName('무야호!');
-        navigation.navigate('QR');
-    }
-    return (
+	const { name, setName } = useContext(UserContext);
+	const [userInfo, setUserInfo] = useState({});
+
+	const signIn = async () => {
+		GoogleSignin.configure({
+			iosClientId: '690007679691-m12da57821qgo9dbp59ud9ssm9nf0kh9.apps.googleusercontent.com',
+		});
+		try {
+			await GoogleSignin.hasPlayServices();
+			const info = await GoogleSignin.signIn();
+			setUserInfo(info);
+			console.log(userInfo);
+			navigation.navigate('QR');
+		} catch (error) {
+			//
+		}
+	};
+
+	const login = () => {
+		setName('무야호!');
+		navigation.navigate('QR');
+	};
+	return (
 		<SafeAreaView style={styles.container}>
 			<View>
 				<Text>login</Text>
-                <Button onPress={login} title='login'/>
+				<Button onPress={login} title="login" />
+				<Button onPress={signIn} title="google" />
 			</View>
 		</SafeAreaView>
 	);
