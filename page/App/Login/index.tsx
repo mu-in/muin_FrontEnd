cce8653394135bbaf97a903b27f737948658ba08
2kyung19 <2kyung19@gmail.com>
2021-10-15 17:15:52 +0900

    update : login page layout [#4]

--- page/App/Login/index.tsx
+++ page/App/Login/index.tsx
@@ -1,5 +1,5 @@
-import React, { ReactElement, useContext } from 'react';
-import { SafeAreaView, View, Text, StyleSheet, Button, Alert, Image } from 'react-native';
+import React, { ReactElement, useContext, useState } from 'react';
+import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity, Alert, Image } from 'react-native';
 import { ParamListBase } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
@@ -17,10 +17,40 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		backgroundColor: '#ffffff',
 	},
+	logo: {
+		width: 100,
+		height: 100,
+		marginBottom: 100,
+	},
+	welcome: {
+		marginBottom: 200,
+	},
+	text: {
+		fontSize: 30,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		lineHeight: 40,
+	},
+	btn: {
+		backgroundColor: '#298FFF',
+		borderWidth: 1,
+		borderColor: '#298FFF',
+		borderRadius: 10,
+		overflow: 'hidden',
+		padding: 8,
+		paddingLeft: 25,
+		paddingRight: 25,
+		color: '#ffffff',
+		fontSize: 25,
+		fontWeight: 'bold',
+	},
 });
 
+const logo = require('../../../img/logo.png');
+
 function Login({ navigation }: Props): ReactElement {
-	const { setName, setGoogle } = useContext(UserContext);
+	const { name, setName, setGoogle } = useContext(UserContext);
+	const [login, setLogin] = useState(false);
 
 	const signIn = async () => {
 		GoogleSignin.configure({
@@ -33,18 +63,35 @@ function Login({ navigation }: Props): ReactElement {
 			if (info.user.name != null) setName(info.user.name);
 			if (info.idToken != null) setGoogle(info.idToken);
 
-			navigation.navigate('QR');
+			setLogin(true);
 		} catch (error) {
-			console.log(error);
+			Alert.alert('로그인 실패', '구글 로그인에 실패했습니다.', [{ text: '확인' }]);
 		}
+	};
+
+	const welcome = () => {
+		navigation.navigate('QR');
 	};
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<View>
-				<Text>login</Text>
-				<Button onPress={signIn} title="google" />
-			</View>
+			{login ? (
+				<>
+					<View style={styles.welcome}>
+						<Text style={styles.text}>{`안녕하세요\n${name}님`}</Text>
+					</View>
+					<TouchableOpacity onPress={welcome}>
+						<Text style={styles.btn}>시작하기</Text>
+					</TouchableOpacity>
+				</>
+			) : (
+				<>
+					<View>
+						<Image source={logo} style={styles.logo} />
+					</View>
+					<GoogleSigninButton onPress={signIn} size={GoogleSigninButton.Size.Standard} />
+				</>
+			)}
 		</SafeAreaView>
 	);
 }
